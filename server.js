9d18a76c6ac2592b086f6ba231617a7e84520761
jaspreet <jk975067@gmail.com>
2024-04-17 23:36:47 +0530

    feat: initial setup with express and node.js

--- server.js
+++ server.js
@@ -1,1 +1,10 @@
-console.log('hi--');
+const express = require('express')
+const app = express()
+
+app.get('/', (req,res) => {
+    res.send('hi')
+})
+
+app.listen(3000, ()=> {
+    console.log('node server is running on port 3000');
+})
